f5407bf668b821d6276776472567bf06ab07aa59
qingyangucb <qingyangucb@gmail.com>
2020-01-08 19:31:45 -0800

    finished hw

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -1,94 +1,90 @@
-function makeMap(earthquake, tectonicplates) {
-	console.log(earthquake);
-	console.log(tectonicplates);
+var tectonicplates_layer = new L.LayerGroup();
+var earthquakes_layer = new L.LayerGroup();
 
-	var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-		maxZoom: 18,
-		id: "mapbox.satellite",
-		accessToken: API_KEY
-	});
+var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+	maxZoom: 18,
+	id: "mapbox.satellite",
+	accessToken: API_KEY
+});
 
-	var grayscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-		maxZoom: 18,
-		id: "mapbox.dark",
-		accessToken: API_KEY
-	});
+var grayscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+	maxZoom: 18,
+	id: "mapbox.dark",
+	accessToken: API_KEY
+});
 
-	var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-		maxZoom: 18,
-		id: "mapbox.outdoors",
-		accessToken: API_KEY
-	});
+var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+	maxZoom: 18,
+	id: "mapbox.outdoors",
+	accessToken: API_KEY
+});
 
-	var baseMaps = {
-		"Satellite Map": satellite,
-		"Grayscale Map": grayscale,
-		"Ourdoors Map": outdoors
-	};
+var baseMaps = {
+	"Satellite Map": satellite,
+	"Grayscale Map": grayscale,
+	"Ourdoors Map": outdoors
+};
 
-	var overlayMaps = {
-		"Fault Lines": tectonicplates,
-		"Earthquakes": earthquake
-	};
-
-
-	var map = L.map("map", {
-		center: [40.73, -74.0059],
-		zoom: 3,
-		layers: [satellite, earthquake]
-	});
+var overlayMaps = {
+	"Fault Lines": tectonicplates_layer,
+	"Earthquakes": earthquakes_layer
+};
 
 
-	L.control.layers(baseMaps, overlayMaps, {
-		collapsed: true
-	}).addTo(map);
+var map = L.map("map", {
+	center: [40.73, -74.0059],
+	zoom: 3,
+	layers: [satellite, earthquakes_layer]
+});
 
-}
+
+L.control.layers(baseMaps, overlayMaps, {
+	collapsed: true
+}).addTo(map);
+
+
+
+
 
 function makeEarthquake(url) {
-	var earthquakeMarkers = [];
+	
 	d3.json(url, function(data) {
 		var features = data.features;
 		
 		for (var index = 0; index < features.length; index++) {
 			var feature = features[index];
 
-			var marker = L.circle([feature.geometry.coordinates[0], feature.geometry.coordinates[1]], {
+			var marker = L.circle([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
 				color: 'black',
 				fillColor: getColor(feature.properties.mag),
 				fillOpacity: 0.5,
 				radius: feature.properties.mag * 10
 			})
-			.bindPopup("<h3>" + feature.title + "</h3>");
-
-			earthquakeMarkers.push(marker);
+			.bindPopup("<h3>" + feature.title + "</h3>")
+			.addTo(earthquakes_layer);			
 		}
-
 		
 	});
 	// console.log(earthquakeMarkers);
-	return earthquakeMarkers;
+	
 }
 
 
 
 function makePlates(url) {
-	var tectonicMarkers = [];
+	
 	d3.json(url, function(data) {
-		var features = data.features;
-		
-		for (var index = 0; index < features.length; index++) {
-			var latlngs = features[index].geometry.coordinates;
-			var polyline = L.polyline(latlngs, {color: 'red'});
-			tectonicMarkers.push(polyline);
-			
-		}
+      L.geoJson(data, {
+        color: "red",
+        weight: 2
+      })
+      .addTo(tectonicplates_layer);
 	})
 	// console.log(tectonicMarkers);
-	return tectonicMarkers;
+	
 
 }
 
@@ -103,13 +99,8 @@ function getColor(d) {
 
 var link1 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
 var link2 = "./static/data/PB2002_plates.json"
-var earthquakes2 = makeEarthquake(link1);
-console.log(earthquakes2);
-var tectonicplates2 = makePlates(link2);
-console.log(tectonicplates2);
-var earthquakes = L.layerGroup([makeEarthquake(link1)]);
-console.log(earthquakes);
-var tectonicplates = L.layerGroup([makePlates(link2)]);
-console.log(tectonicplates);
-makeMap(earthquakes, tectonicplates); 
+makeEarthquake(link1);
+makePlates(link2);
+earthquakes_layer.addTo(map);
+tectonicplates_layer.addTo(map);
 
